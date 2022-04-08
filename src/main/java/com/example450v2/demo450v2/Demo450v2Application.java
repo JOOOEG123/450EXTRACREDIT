9a98eba7caae62343a1cfbaab083705426cca319
JOOOEG123 <57308580+JOOOEG123@users.noreply.github.com>
2022-04-08 11:29:38 -0400

    Update

---
+++ src/main/java/com/example450v2/demo450v2/Demo450v2Application.java
@@ -0,0 +1,13 @@
+package com.example450v2.demo450v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo450v2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo450v2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example450v2/demo450v2/Demo450v2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example450v2.demo450v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo450v2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
